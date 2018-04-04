a5add912703835c115075b0ff0e96ef5dc230557
Mike Mathew <m2mathew@me.com>
2018-04-03 21:31:58 -0500

    Adds styles to title

---
+++ src/components/title/index.js
@@ -0,0 +1,49 @@
+// External Dependencies
+import React from 'react';
+
+// Local Variables
+const titleStyles = {
+  width: '100%',
+	marginTop: '5em',
+	marginBottom: '0.5em',
+	padding: '0.5em auto',
+	borderBottom: '6px solid gray',
+};
+
+const h1Styles = {
+  fontFamily: 'Open Sans, sans-serif',
+	fontWeight: '300',
+	textTransform: 'uppercase',
+	fontSize: 30,
+	textAlign: 'center',
+};
+
+const h3Styles = {
+  fontFamily: 'Open Sans, sans-serif',
+	fontWeight: 100,
+	textTransform: 'lowercase',
+	fontSize: 22,
+	marginBottom: '1.5em',
+	marginLeft: '0.3em',
+	textAlign: 'center',
+};
+
+// Component Definition
+const Title = () => (
+  <div css={titleStyles}>
+    <h1
+      className="text-left"
+      css={h1Styles}
+    >
+      Mike Mathew
+    </h1>
+    <h3
+      className="text-left"
+      css={h3Styles}
+    >
+      front end web developer
+    </h3>
+  </div>
+);
+
+export default Title;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,14 @@
+// External Dependencies
 import React from 'react'
 import Link from 'gatsby-link'
 
+// Internal Dependencies
+import Title from '../components/title';
+
+// Component Definition
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
+    <Title />
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>

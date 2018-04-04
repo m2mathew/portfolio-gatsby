764b77e6f9f8c9607075a52b8e0a635016c3c587
Mike Mathew <m2mathew@me.com>
2018-04-03 21:45:02 -0500

    Updates gatsby-config with glamor and react-next

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,11 @@
 module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
+    siteUrl: `http://www.mikemathew.com`,
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    `gatsby-plugin-glamor`,
+    `gatsby-plugin-react-next`,
+  ],
 };
